8a52f81a10cf38c5fe03cfa9613684c414b6b8cc
Theodor Risager <theodor349@gmail.com>
2022-02-17 13:28:10 +0100

    Init

---
+++ src/Parser/OpenDataParser/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Generate Intermediate Object
+// Decide what type of dataset it is 
+    // public enum DatasetType { Unknows = 0, Partking = 1 }
+// Call correct parser
+// Store in database 
