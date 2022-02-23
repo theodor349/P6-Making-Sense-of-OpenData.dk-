cb46a38aa57d82ee40f070479ab14d0fab10abc9
EmilGHenriksen <56338371+EmilGHenriksen@users.noreply.github.com>
2022-02-23 14:36:47 +0100

    De andre sagde vi skulle pushe

--- src/Parser/IntermediateGenerator/IntermediateGenerator.cs
+++ src/Parser/IntermediateGenerator/IntermediateGenerator.cs
@@ -1,12 +1,41 @@
-﻿using Shared.Models;
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Shared.Models;
+using System.IO;
 
 namespace IntermediateGenerator
 {
     public class IntermediateGenerator : IIntermediateGenerator
     {
+        private readonly ILogger<IntermediateGenerator> _logger;
+
+        public IntermediateGenerator(ILogger<IntermediateGenerator> logger)
+        {
+            _logger = logger;
+        }
         public Task<DatasetObject> GenerateAsync()
         {
+            ParseFile(new FileInfo("C:\\Users\\Emil-\\Desktop\\Dataset parking\\34.20.12_Parkeringsarealer.geojson"));
             return Task.FromResult(new DatasetObject());
+        }
+        public void ParseFile(FileInfo file)
+        {
+            if (file.Extension == ".geojson")
+            {
+                ParseJson(file);
+            }
+            else
+            {
+                _logger.LogError("Wrong file format " + file.Extension);
+            }
+        }
+
+        private void ParseJson(FileInfo file)
+        {
+            string jsonString = File.ReadAllText(file.FullName);
+            JObject jsonObject = JObject.Parse(jsonString);
+            string prop = jsonObject.SelectToken("$.features.EJER").Value<string>();
+            _logger.LogInformation(prop);
         }
     }
 }
