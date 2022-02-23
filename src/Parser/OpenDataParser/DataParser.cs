4b53dc265585cf424ecbc0fa0fb807b34cecd30a
420skaterboy <l.enggaard.rasmussen@gmail.com>
2022-02-23 13:34:46 +0100

    Program setup

---
+++ src/Parser/OpenDataParser/DataParser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDataParser
+{
+    internal interface IDataParser
+    {
+        Task Run();
+    }
+
+    internal class DataParser : IDataParser
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<DataParser> _logger;
+
+        public DataParser(IConfiguration configuration, ILogger<DataParser> logger) 
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task Run()
+        {
+            _logger.LogInformation("Hello World");
+            _logger.LogInformation(_configuration["HelloWorldString"]);
+        }
+    }
+    
+}
--- src/Parser/OpenDataParser/Program.cs
+++ src/Parser/OpenDataParser/Program.cs
@@ -1,8 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenDataParser;
+using Serilog;
 
-// Generate Intermediate Object
-// Decide what type of dataset it is 
-    // public enum DatasetType { Unknows = 0, Partking = 1 }
-// Call correct parser
-// Store in database 
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+
+Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+Log.Logger.Information("Application Starting");
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddTransient<IDataParser, DataParser>();
+    })
+    .UseSerilog()
+    .Build();
+
+var svc = ActivatorUtilities.CreateInstance<DataParser>(host.Services);
+svc.Run();
+
+void BuildConfig(ConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+        .AddEnvironmentVariables();
+}
