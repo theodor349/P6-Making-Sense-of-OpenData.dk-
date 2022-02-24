5233d34c3bb4852e66b96ef6d2635f8dc8977248
EmilGHenriksen <56338371+EmilGHenriksen@users.noreply.github.com>
2022-02-24 08:48:32 +0100

    Split intermediate generator in two

---
+++ src/Parser/IntermediateGenerator/Parse file/ParseJson.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.ComponentInterfaces;
+using Microsoft.Extensions.Logging;
+
+namespace IntermediateGenerator.Parse_file
+{
+    internal class ParseJson
+    {
+        private readonly ILogger<IntermediateGenerator> _logger;
+        public ParseJson(ILogger<IntermediateGenerator> logger)
+        {
+            _logger = logger;
+        }
+        public void ParseFile(FileInfo file)
+        {
+            if (file.Extension == ".geojson")
+            {
+                ParseJson(file);
+            }
+            else
+            {
+                _logger.LogError("Wrong file format " + file.Extension);
+            }
+        }
+
+        private void ParseJson(FileInfo file)
+        {
+            string jsonString = File.ReadAllText(file.FullName);
+            JObject jsonObject = JObject.Parse(jsonString);
+            string prop = (string)jsonObject.SelectToken("$.features[0].properties.EJER");
+            _logger.LogInformation(prop);
+        }
+    }
+}
