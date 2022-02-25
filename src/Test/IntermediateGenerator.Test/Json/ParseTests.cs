e5711037c630073a218faa88420566ed5a718547
420skaterboy <l.enggaard.rasmussen@gmail.com>
2022-02-25 13:01:31 +0100

    Parse_OneNested_CorrectOutput

--- src/Test/IntermediateGenerator.Test/Json/ParseTests.cs
+++ src/Test/IntermediateGenerator.Test/Json/ParseTests.cs
@@ -16,7 +16,7 @@ namespace IntermediateGenerator.Test.Json
     public class ParseTests
     {
         [TestMethod]
-        public void ParseTest()
+        public void Parse_NonNested_CorrectOutput()
         {
             string fileName = "fileName";
             string fileExtension = ".geojson";
@@ -37,6 +37,43 @@ namespace IntermediateGenerator.Test.Json
                 new TextAttribute("attr3", "Three"),
             }));
             var expected = new DatasetObject(fileExtension, fileName, objects);
+
+            var parser = setup.GetParseJson();
+            var task = parser.Parse(inputString, fileExtension, fileName);
+            task.Wait();
+            var res = task.Result;
+
+            res.Should().BeEquivalentTo(expected);
+        }
+        [TestMethod]
+        public void Parse_OneNested_CorrectOutput()
+        {
+            string fileName = "fileName";
+            string fileExtension = ".geojson";
+            var nestedObj = new
+            {
+                attr1 = "One",
+                attr2 = "Two",
+                attr3 = "Three",
+            };
+            var jsonObj = new
+            {
+                nestedObj = nestedObj,
+            };
+            string inputString = JsonConvert.SerializeObject(jsonObj);
+            var setup = new TestSetup();
+
+            var objects = new List<IntermediateObject>();
+            objects.Add(new IntermediateObject(new List<ObjectAttribute>()
+            {
+                new ListAttribute("nestedObj", new List<ObjectAttribute>()
+                {
+                    new TextAttribute("attr1", "One"),
+                    new TextAttribute("attr2", "Two"),
+                    new TextAttribute("attr3", "Three"),
+                }),
+            }));
+            var expected = new DatasetObject(fileExtension.ToLower(), fileName.ToLower(), objects);
 
             var parser = setup.GetParseJson();
             var task = parser.Parse(inputString, fileExtension, fileName);
