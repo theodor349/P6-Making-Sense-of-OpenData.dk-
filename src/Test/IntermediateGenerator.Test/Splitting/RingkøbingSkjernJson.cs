6b37eb8d3f39baf332c5772c9a305c364ef83937
EmilGHenriksen <56338371+EmilGHenriksen@users.noreply.github.com>
2022-03-02 13:38:57 +0100

    Added test for Odense dataset, aswell as a header removed test

--- src/Test/IntermediateGenerator.Test/Splitting/RingkøbingSkjernJson.cs
+++ src/Test/IntermediateGenerator.Test/Splitting/RingkøbingSkjernJson.cs
@@ -34,5 +34,62 @@ namespace IntermediateGenerator.Test.Splitting
 
             res.Should().BeEquivalentTo(expectedDataset);
         }
+
+        [DataRow(2)]
+        [DataRow(3)]
+        [TestMethod]
+        public void Split_RingkøbingHeader_Removed(int amount)
+        {
+            var attributes = new List<ObjectAttribute>();
+            attributes.Add(new TextAttribute("type", "FeatureCollection"));
+            attributes.Add(new TextAttribute("name", "34.20.12_Parkeringsarealer"));
+            attributes.Add(new ListAttribute("crs", new List<ObjectAttribute>()
+            {
+                new TextAttribute("type", "mame"),
+                new ListAttribute("properties", new List<ObjectAttribute>()
+                {
+                    new TextAttribute("name", "urn:ogc:def:crs:OGC:1.3:CRS84")
+                })
+            }));
+            var featureItems = ModelFactory.GetListOfObjectsAttributes(amount, ModelFactory.GetRingkøbingSkjernParking);
+            var features = new ListAttribute("features", featureItems);
+            attributes.Add(features);
+            var intermediateObject = ModelFactory.GetIntermediateObject(attributes);
+            var inputDataset = ModelFactory.GetDatasetObject(intermediateObject);
+
+            var parkingSpots = ModelFactory.GetListOfObjectsAttributes(amount, ModelFactory.GetRingkøbingSkjernParking);
+            var expectedIntermediate = ModelFactory.GetIntermediateObject(parkingSpots);
+            var expectedDataset = ModelFactory.GetDatasetObject(expectedIntermediate);
+
+            var setup = new TestSetup();
+            var splitter = setup.IntermediateObjectSplitter();
+            var res = splitter.SplitObject(inputDataset);
+
+            res.Should().BeEquivalentTo(expectedDataset);
+        }
+
+        [DataRow(2)]
+        [DataRow(3)]
+        [TestMethod]
+        public void Split_OdenseHeader_Removed(int amount)
+        {
+            var attributes = new List<ObjectAttribute>();
+            attributes.Add(new TextAttribute("type", "FeatureCollection"));
+            var featureItems = ModelFactory.GetListOfObjectsAttributes(amount, ModelFactory.GetRingkøbingSkjernParking);
+            var features = new ListAttribute("features", featureItems);
+            attributes.Add(features);
+            var intermediateObject = ModelFactory.GetIntermediateObject(attributes);
+            var inputDataset = ModelFactory.GetDatasetObject(intermediateObject);
+
+            var parkingSpots = ModelFactory.GetListOfObjectsAttributes(amount, ModelFactory.GetRingkøbingSkjernParking);
+            var expectedIntermediate = ModelFactory.GetIntermediateObject(parkingSpots);
+            var expectedDataset = ModelFactory.GetDatasetObject(expectedIntermediate);
+
+            var setup = new TestSetup();
+            var splitter = setup.IntermediateObjectSplitter();
+            var res = splitter.SplitObject(inputDataset);
+
+            res.Should().BeEquivalentTo(expectedDataset);
+        }
     }
 }
