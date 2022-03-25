f499a0fb3445330533f335edf805f22764594ae0
Gajhede <Hrgajhede@gmail.com>
2022-03-25 10:26:56 +0100

    sorter

--- src/Parser/DatasetParser/ParseToJson.cs
+++ src/Parser/DatasetParser/ParseToJson.cs
@@ -20,9 +20,12 @@ namespace DatasetParser
         public float longitude;
     }
 
+
     public class ParseToJson : IParseToJson
     {
         private readonly IConfiguration _configuration;
+
+        private Coordinate firstCoordinate;
 
 
         public ParseToJson(IConfiguration configuration)
@@ -132,7 +135,8 @@ namespace DatasetParser
             {
                 coords.Add(GetCoordinate(coord));
             }
-            SortAccordingToRightHandRule(coords);
+            //  under construction
+            coords = SortAccordingToRightHandRule(coords);
 
             foreach(Coordinate coord in coords)
             {
@@ -152,12 +156,93 @@ namespace DatasetParser
             return new Coordinate{ lattitude = coord1, longitude = coord2};
         }
 
-        private void SortAccordingToRightHandRule(List<Coordinate> coords)
+        private List<Coordinate> SortAccordingToRightHandRule(List<Coordinate> coords)
         {
+            // A linear ring MUST follow the right-hand rule with respect to the area it bounds, i.e.,
+            // exterior rings are counterclockwise, and holes are clockwise.
 
+            // all elements must be ubique for Sorting scan, therefore remove the duplicate required by geoJson
+            coords.RemoveAt(coords.Count -1);
+
+            // lattitude = y
+            int firstCoordinateIndex = 0;
+            firstCoordinate = coords[0];
+            float lowestLattitude = float.PositiveInfinity;
+
+            int currentIndex = 0;
+            foreach (Coordinate item in coords)
+            {
+                if(item.lattitude < lowestLattitude || item.lattitude == lowestLattitude && item.longitude < firstCoordinate.longitude)
+                {
+                    lowestLattitude = item.lattitude;
+                    firstCoordinate = item;
+                    firstCoordinateIndex = currentIndex;
+                }
+                currentIndex++;
+            }
+
+            // remove first coord from list and reinsert at index 0
+            coords.RemoveAt(firstCoordinateIndex);
+            coords.Sort((coord1, coord2) =>
+           {
+               return Less(coord1, coord2);
+           });
+
+            // innsert first and last duplicate point
+            coords.Insert(0, firstCoordinate);
+            coords.Add(firstCoordinate);
+            return coords;
         }
 
+        private int Less(Coordinate a, Coordinate b)
+        {
+            if (a.longitude - firstCoordinate.longitude >= 0 && b.longitude - firstCoordinate.longitude < 0)
+                return 1;
+            if (a.longitude - firstCoordinate.longitude < 0 && b.longitude - firstCoordinate.longitude >= 0)
+                return -1;
+            if (a.longitude - firstCoordinate.longitude == 0 && b.longitude - firstCoordinate.longitude == 0)
+            {
+                if (a.lattitude - firstCoordinate.lattitude >= 0 || b.lattitude - firstCoordinate.lattitude >= 0)
+                {
+                    if (a.lattitude > b.lattitude)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                if (b.lattitude > a.lattitude)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
 
+            // compute the cross product of vectors (center -> a) x (center -> b)
+            float det = (a.longitude - firstCoordinate.longitude) * (b.lattitude - firstCoordinate.lattitude) - (b.longitude - firstCoordinate.longitude) * (a.lattitude - firstCoordinate.lattitude);
+            if (det > 0)
+                return 1;
+            if (det < 0)
+                return -1;
+
+            // points a and b are on the same line from the center
+            // check which point is closer to the center
+            float d1 = (a.longitude - firstCoordinate.longitude) * (a.longitude - firstCoordinate.longitude) + (a.lattitude - firstCoordinate.lattitude) * (a.lattitude - firstCoordinate.lattitude);
+            float d2 = (b.longitude - firstCoordinate.longitude) * (b.longitude - firstCoordinate.longitude) + (b.lattitude - firstCoordinate.lattitude) * (b.lattitude - firstCoordinate.lattitude);
+            if (d1 > d2)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
 
