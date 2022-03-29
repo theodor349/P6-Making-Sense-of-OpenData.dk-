f507db5f2e917160b6b19581ce56461e7c8678dc
420skaterboy <l.enggaard.rasmussen@gmail.com>
2022-03-29 11:07:16 +0200

    hej

---
+++ src/DatasetParser.Test/JsonTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using DatasetParser.Test.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using Shared.Models;
+using Shared.Models.ObjectAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatasetParser.Test
+{
+    [TestClass]
+    public class JsonTests
+    {
+        [TestMethod]
+        public void Test_ParseToJson_Correct()
+        {
+            //var expected;
+        }
+    }
+}
