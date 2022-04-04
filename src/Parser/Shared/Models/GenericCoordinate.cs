1db47cb6c4ac95ef1a99be55b741141e4c250753
Theodor Risager <theodor349@gmail.com>
2022-04-04 12:38:47 +0200

    Right Hand Rule work for both Clock and Anti Clock

--- src/Parser/Shared/Models/GenericCoordinate.cs
+++ src/Parser/Shared/Models/GenericCoordinate.cs
@@ -73,112 +73,16 @@ namespace Shared.Models
 
         public static List<GenericCoordinate> SortAccordingToRightHandRule(List<GenericCoordinate> coords)
         {
-            coords.RemoveAt(coords.Count - 1);
-            int lowestCoordinateIndex = GetFirstCoordinate(coords);
-
-            if (AreCoordsSortedRightHandRule(coords, lowestCoordinateIndex) == false)
+            double sum = 0;
+            for (var i = 0; i < (coords.Count - 1); i++)
+            {
+                sum += (coords[i + 1].latitude - coords[i].latitude) * (coords[i + 1].longitude + coords[i].longitude);
+            }
+            if (sum > 0)
             {
                 coords.Reverse();
             }
-            coords.Add(coords[0]);
             return coords;
-        }
-
-        private static bool AreCoordsSortedRightHandRule(List<GenericCoordinate> coords, int lowestCoordinateIndex)
-        {
-            GenericCoordinate firstCoordinate = coords[lowestCoordinateIndex];
-            GenericCoordinate a;
-            GenericCoordinate b;
-
-            if (lowestCoordinateIndex != coords.Count() - 1)
-                b = coords[lowestCoordinateIndex + 1];
-            else
-                b = coords[0];
-
-            if (lowestCoordinateIndex != 0)
-                a = coords[lowestCoordinateIndex - 1];
-            else
-                a = coords.LastOrDefault();
-
-            int i = 0;
-            while(a.Equals(b) && i < coords.Count)
-            {
-                int index = lowestCoordinateIndex + 1 + i;
-                if (index >= coords.Count)
-                    index -= coords.Count;
-                b = coords[index];
-                i++;
-            }
-
-
-            if (a.longitude - firstCoordinate.longitude >= 0 && b.longitude - firstCoordinate.longitude < 0)
-                return true;
-            if (a.longitude - firstCoordinate.longitude < 0 && b.longitude - firstCoordinate.longitude >= 0)
-                return false;
-            if (a.longitude - firstCoordinate.longitude == 0 && b.longitude - firstCoordinate.longitude == 0)
-            {
-                if (a.latitude - firstCoordinate.latitude >= 0 || b.latitude - firstCoordinate.latitude >= 0)
-                {
-                    if (a.latitude > b.latitude)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-                if (b.latitude > a.latitude)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-
-            // compute the cross product of vectors (center -> a) x (center -> b)
-            double det = (a.longitude - firstCoordinate.longitude) * (b.latitude - firstCoordinate.latitude) - (b.longitude - firstCoordinate.longitude) * (a.latitude - firstCoordinate.latitude);
-            if (det > 0)
-                return true;
-            if (det < 0)
-                return false;
-
-            // points a and b are on the same line from the center
-            // check which point is closer to the center
-            double d1 = (a.longitude - firstCoordinate.longitude) * (a.longitude - firstCoordinate.longitude) + (a.latitude - firstCoordinate.latitude) * (a.latitude - firstCoordinate.latitude);
-            double d2 = (b.longitude - firstCoordinate.longitude) * (b.longitude - firstCoordinate.longitude) + (b.latitude - firstCoordinate.latitude) * (b.latitude - firstCoordinate.latitude);
-            if (d1 > d2)
-            {
-                return true;
-            }
-            else
-            {
-                throw new Exception();
-            }
-        }
-
-        private static int GetFirstCoordinate(List<GenericCoordinate> coords)
-        {
-
-            int firstCoordinateIndex = 0;
-            GenericCoordinate firstCoordinate = coords[0];
-            double lowestLatitude = double.PositiveInfinity;
-
-            int currentIndex = 0;
-            foreach (GenericCoordinate item in coords)
-            {
-                if (item.latitude < lowestLatitude || item.latitude == lowestLatitude && item.longitude < firstCoordinate.longitude)
-                {
-                    lowestLatitude = item.latitude;
-                    firstCoordinate = item;
-                    firstCoordinateIndex = currentIndex;
-                }
-                currentIndex++;
-            }
-
-            return firstCoordinateIndex;
         }
     }
 }
