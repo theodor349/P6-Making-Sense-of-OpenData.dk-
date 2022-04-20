551b5d3f4e65c6fadbd7b2af57c45dd2291ef767
EmilGHenriksen <56338371+EmilGHenriksen@users.noreply.github.com>
2022-04-20 13:48:37 +0200

    Made Synonym finder

--- src/Parser/LabelRecognizer/LabelNameLookupTable.cs
+++ src/Parser/LabelRecognizer/LabelNameLookupTable.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
+using NHunspell;
 using Shared.Models;
 using Shared.Models.ObjectAttributes;
 using System.Text.Json;
 
 namespace LabelRecognizer
 {
-    public enum TargetKey { Count, Address }
     public enum LookupLanguages { DK, ENG }
 
     class LabelNameLookupTable : ILabelNameLookupTable
@@ -20,6 +20,9 @@ namespace LabelRecognizer
         }
         public Task AssignLabels(DatasetObject dataset)
         {
+           // FindSynonym("blomst", LookupLanguages.DK);
+           // FindSynonym("park", LookupLanguages.ENG);
+
             foreach (var obj in dataset.Objects)
             {
                 foreach (var attr in obj.Attributes)
@@ -67,12 +70,56 @@ namespace LabelRecognizer
                     {
                         if (attr.Name.Contains(value, StringComparison.InvariantCultureIgnoreCase))
                         {
-                            return new Tuple<bool, float, ObjectLabel>(true, 1, (ObjectLabel) Enum.Parse(typeof(ObjectLabel), target.Target));
+                            return new Tuple<bool, float, ObjectLabel>(true, 1, (ObjectLabel)Enum.Parse(typeof(ObjectLabel), target.Target));
                         }
                     }
                 }
             }
             return new Tuple<bool, float, ObjectLabel>(false, 0, new ObjectLabel());
+        }
+
+        public void FindSynonym(string inputWord, LookupLanguages lang)
+        {
+            if (lang == LookupLanguages.DK)
+            {
+                Hunspell hunspellDanish = new Hunspell("da_dk.aff", "da_dk.dic");
+                MyThes thesDanish = new MyThes("th_da_dk.dat");
+
+                ThesResult resDanish = thesDanish.Lookup(inputWord, hunspellDanish);
+                if (resDanish.IsGenerated)
+                {
+                    Console.WriteLine("Generated over stem (The original word form wasn't in the thesaurus)");
+                }
+                foreach (ThesMeaning meaning in resDanish.Meanings)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("  DK Meaning: " + meaning.Description);
+                    foreach (string synonym in meaning.Synonyms)
+                    {
+                        Console.WriteLine("    DK Synonym: " + synonym);
+                    }
+                }
+            } 
+            else if (lang == LookupLanguages.ENG)
+            {
+                Hunspell hunspellEnglish = new Hunspell("en_us.aff", "en_us.dic");
+                MyThes thesEnglish = new MyThes("th_en_us_v2.dat");
+
+                ThesResult resEnglish = thesEnglish.Lookup(inputWord, hunspellEnglish);
+                if (resEnglish.IsGenerated)
+                {
+                    Console.WriteLine("Generated over stem (The original word form wasn't in the thesaurus)");
+                }
+                foreach (ThesMeaning meaning in resEnglish.Meanings)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("  ENG Meaning: " + meaning.Description);
+                    foreach (string synonym in meaning.Synonyms)
+                    {
+                        Console.WriteLine("    ENG Synonym: " + synonym);
+                    }
+                }
+            }
         }
 
         private LookupTable GenerateLookuptable(string lookupTablePath)
