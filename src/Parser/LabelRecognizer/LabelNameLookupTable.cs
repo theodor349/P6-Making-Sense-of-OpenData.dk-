b38e2902bebc20c8244991e5cd5764d7adfe71ee
EmilGHenriksen <56338371+EmilGHenriksen@users.noreply.github.com>
2022-04-22 09:14:36 +0200

    Made dictionary singletons in constructor and made sure to only lookup each intermediate object attr once in the thesaurus

--- src/Parser/LabelRecognizer/LabelNameLookupTable.cs
+++ src/Parser/LabelRecognizer/LabelNameLookupTable.cs
@@ -12,16 +12,26 @@ namespace LabelRecognizer
     {
         private readonly IConfiguration _configuration;
         private readonly LookupTable _lookupTable;
+        private readonly Hunspell _hunspellDanish;
+        private readonly MyThes _thesDanish;
+        private readonly Hunspell _hunspellEnglish;
+        private readonly MyThes _thesEnglish;
 
         public LabelNameLookupTable(IConfiguration configuration)
         {
             _configuration = configuration;
             _lookupTable = GenerateLookuptable(configuration["Input:LabelNameLookupTablePath"]);
+
+            _hunspellDanish = new Hunspell("Dictionaries/da_dk.aff", "Dictionaries/da_dk.dic");
+            _hunspellEnglish = new Hunspell("Dictionaries/en_us.aff", "Dictionaries/en_us.dic");
+            _thesDanish = new MyThes("Dictionaries/th_da_dk.dat");
+            _thesEnglish = new MyThes("Dictionaries/th_en_us_v2.dat");
+
         }
         public Task AssignLabels(DatasetObject dataset)
         {
            // FindSynonym("blomst", LookupLanguages.DK);
-            FindSynonym("park", LookupLanguages.ENG);
+           // FindSynonym("park", LookupLanguages.ENG);
 
             foreach (var obj in dataset.Objects)
             {
@@ -35,13 +45,13 @@ namespace LabelRecognizer
 
         private void SetLabels(ObjectAttribute attr)
         {
-            // ASSIGN Actual labels
-            AssignLabelFromLookup(attr);
+            if (ContainsPredefinedName(attr))
+            {
+                AssignLabelFromLookup(attr);
+            }
+            
             if (attr.GetType() == typeof(ListAttribute))
             {
-                // Get children
-                //   for each child
-                //   SetLabels
                 var children = (List<ObjectAttribute>)attr.Value;
                 foreach (var child in children)
                 {
@@ -50,76 +60,127 @@ namespace LabelRecognizer
             }
         }
 
+        private bool ContainsPredefinedName(ObjectAttribute attr)
+        {
+            switch (attr.Name)
+            {
+                // StartArray and StartObject won't have Value appended
+                case "StartObject":
+                case "StartArray":
+                case "FloatValue":
+                case "NoneValue":
+                case "StartConstructorValue":
+                case "PropertyNameValue":
+                case "CommentValue":
+                case "RawValue":
+                case "IntegerValue":
+                case "StringValue":
+                case "BooleanValue":
+                case "NullValue":
+                case "UndefinedValue":
+                case "EndObjectValue":
+                case "EndArrayValue":
+                case "EndConstructorValue":
+                case "DateValue":
+                case "BytesValue":
+                case "DoubleValue":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
         private void AssignLabelFromLookup(ObjectAttribute attr)
         {
             var labelTuple = Lookup(attr);
             // If a value is found
-            if (labelTuple.Item1)
+            foreach (var tuple in labelTuple)
             {
-                attr.AddLabel(labelTuple.Item3, labelTuple.Item2);
+                attr.AddLabel(tuple.Item2, tuple.Item1);
             }
         }
 
-        private Tuple<bool, float, ObjectLabel> Lookup(ObjectAttribute attr)
+        private List<Tuple<float, ObjectLabel>> Lookup(ObjectAttribute attr)
         {
+            var list = new List<Tuple<float, ObjectLabel>>();
+            var hasBeenSearched = new List<string>();
+
             foreach (var target in _lookupTable.LookupTargets)
             {
+                bool targetFound = false;
                 foreach (var lang in target.Languages)
                 {
+                    if (targetFound == true)
+                    {
+                        break;
+                    }
                     foreach (var value in lang.Values)
                     {
+                        List<string> synonyms = new List<string>();
                         if (attr.Name.Contains(value, StringComparison.InvariantCultureIgnoreCase))
                         {
-                            return new Tuple<bool, float, ObjectLabel>(true, 1, (ObjectLabel)Enum.Parse(typeof(ObjectLabel), target.Target));
+                            list.Add(new Tuple<float, ObjectLabel>(1f, (ObjectLabel)Enum.Parse(typeof(ObjectLabel), target.Target)));
+                            targetFound = true;
+                            break;
                         }
+                        else
+                        {
+                            if (lang.Language == "ENG" && !hasBeenSearched.Contains(attr.Name))
+                            {
+                                synonyms = FindSynonyms(value, LookupLanguages.ENG);
+                                hasBeenSearched.Add(attr.Name);
+                            }
+                            else if (lang.Language == "DK" && !hasBeenSearched.Contains(attr.Name))
+                            {
+                                synonyms = FindSynonyms(value, LookupLanguages.DK);
+                                hasBeenSearched.Add(attr.Name);
+                            }
+                            foreach (var synonym in synonyms)
+                            {
+                                if (attr.Name.Contains(synonym, StringComparison.InvariantCultureIgnoreCase))
+                                {
+                                    list.Add(new Tuple<float, ObjectLabel>(0.5f, (ObjectLabel)Enum.Parse(typeof(ObjectLabel), target.Target)));
+                                }
+                            }
+                        }  
                     }
                 }
             }
-            return new Tuple<bool, float, ObjectLabel>(false, 0, new ObjectLabel());
+            return list;
         }
 
-        public void FindSynonym(string inputWord, LookupLanguages lang)
+        public List<string> FindSynonyms(string inputWord, LookupLanguages lang)
         {
+            List<string> listOfSynonyms = new List<string>();
             if (lang == LookupLanguages.DK)
-            {
-                Hunspell hunspellDanish = new Hunspell("Dictionaries/da_dk.aff", "Dictionaries/da_dk.dic");
-                MyThes thesDanish = new MyThes("Dictionaries/th_da_dk.dat");
-
-                ThesResult resDanish = thesDanish.Lookup(inputWord, hunspellDanish);
-                if (resDanish.IsGenerated)
+            {   
+                ThesResult resDanish = _thesDanish.Lookup(inputWord, _hunspellDanish);
+                if (resDanish != null)
                 {
-                    Console.WriteLine("Generated over stem (The original word form wasn't in the thesaurus)");
-                }
-                foreach (ThesMeaning meaning in resDanish.Meanings)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("  DK Meaning: " + meaning.Description);
-                    foreach (string synonym in meaning.Synonyms)
+                    foreach (ThesMeaning meaning in resDanish.Meanings)
                     {
-                        Console.WriteLine("    DK Synonym: " + synonym);
+                        foreach (string synonym in meaning.Synonyms)
+                        {
+                            listOfSynonyms.Add(synonym);
+                        }
                     }
                 }
             } 
             else if (lang == LookupLanguages.ENG)
             {
-                Hunspell hunspellEnglish = new Hunspell("Dictionaries/en_us.aff", "Dictionaries/en_us.dic");
-                MyThes thesEnglish = new MyThes("Dictionaries/th_en_us_v2.dat");
-
-                ThesResult resEnglish = thesEnglish.Lookup(inputWord, hunspellEnglish);
-                if (resEnglish.IsGenerated)
+                ThesResult resEnglish = _thesEnglish.Lookup(inputWord, _hunspellEnglish);
+                if (resEnglish != null)
                 {
-                    Console.WriteLine("Generated over stem (The original word form wasn't in the thesaurus)");
-                }
-                foreach (ThesMeaning meaning in resEnglish.Meanings)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("  ENG Meaning: " + meaning.Description);
-                    foreach (string synonym in meaning.Synonyms)
+                    foreach (ThesMeaning meaning in resEnglish.Meanings)
                     {
-                        Console.WriteLine("    ENG Synonym: " + synonym);
+                        foreach (string synonym in meaning.Synonyms)
+                        {
+                            listOfSynonyms.Add(synonym);
+                        }
                     }
                 }
             }
+            return listOfSynonyms;
         }
 
         private LookupTable GenerateLookuptable(string lookupTablePath)
